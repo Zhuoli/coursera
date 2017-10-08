fd023a9351668dce6ab5df37f9c4d70ec9d18588
Zhuoli <zhuoliseattle@gmail.com>
2017-10-08 02:12:07 -0700

    Week 3

---
+++ Responsive-website-specialization/meteorjs/week_3_peer_assessment/main.js
@@ -0,0 +1,43 @@
+// shared code
+Websites = new Mongo.Collection("websites");
+
+// client only code
+if (Meteor.isClient){
+    // event listeners on the addSiteForm template
+    Template.addSiteForm.events({
+        // this runs when they click the add button... you need to compete it
+     'click .js-add-site':function(event){
+         var url = $('#url_input').val();// get the form value using jquery...
+         var  user = "anonymous";
+         // PUT YOUR CODE HERE!!
+         // set up the user variable so that it contains
+         // the string anonymous person if they are not logged in
+         // or their email address if they are logged in
+             if (Meteor.user()){
+                user = Meteor.user().emails[0].address;
+            }
+
+         var site = {"url":url,
+                     "createdOn":new Date(),
+                     "createdBy":user};
+         Websites.insert(site);
+         return false;
+     }
+    });
+
+    // this helper gets the data from the collection for the site-list Template
+    Template.siteList.helpers({
+        'all_websites':function(){
+            return Websites.find({});
+        },
+        'safer_email':function(email){
+            if (email.indexOf('@')!=-1){// we have an email
+                return email.split('@')[0];
+            }
+            else{// probably anonymouse.
+                return email;
+            }
+        }
+    });
+
+}
